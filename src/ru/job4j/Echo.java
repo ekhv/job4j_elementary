cfb33d4aff9bddee07f5c8bf7d1d32f40b686b07
Khryashchev Evgeniy Olegovich <evgeniy.khryashchev@rosbank.ru>
2021-08-08 20:39:30 +0500

    test echo with Unix time output

---
+++ src/ru/job4j/Echo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+import java.util.Date;
+
+public class Echo {
+    public static void main(String [] args) {
+        System.out.println("Unix Epoch: " + (new Date()).getTime());
+    }
+}
