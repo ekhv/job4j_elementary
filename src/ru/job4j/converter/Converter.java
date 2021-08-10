bb2676c023296074448ef75af6ab104d9b799d15
Khryashchev Evgeniy Olegovich <evgeniy.khryashchev@rosbank.ru>
2021-08-10 22:30:49 +0500

    0. What is testing

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -14,7 +14,15 @@ public class Converter {
         float euro = Converter.rubleToEuro(140);
         System.out.println("140 rubles are " + euro + " euro.");
 
+        float expectedResultEUR = 2;
+        boolean passedResultEUR = expectedResultEUR == Converter.rubleToEuro(140);
+        System.out.println("140 rubles are 2 EUR. Test result : " + passedResultEUR);
+
         float dollar = Converter.rubleToDollar(140);
         System.out.println("140 rubles are " + dollar + " dollar.");
+
+        float expectedResultUSD = 2.3333333f;
+        boolean passedResultUSD = expectedResultUSD == Converter.rubleToDollar(140);
+        System.out.println("140 rubles are 2.3333333 USD. Test result : " + passedResultUSD);
     }
 }
