fe0ea691f1ecba039c2e2d4ed803b758d790bfb3
Khryashchev Evgeniy Olegovich <evgeniy.khryashchev@rosbank.ru>
2021-08-12 10:40:09 +0500

    4. Tests for the sides of a rectangle

---
+++ test/ru/job4j/condition/SqAreaTest.java
@@ -0,0 +1,52 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SqAreaTest {
+
+    @Test
+    public void whenP6K2() {
+        int expected = 2;
+        int p = 6;
+        double k = 2;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenP2K2() {
+        double expected = 0.2755102040816326;
+        int p = 3;
+        double k = 6;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenP10K7() {
+        double expected = 2.734375;
+        int p = 10;
+        double k = 7;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenP5K3() {
+        double expected = 1.171875;
+        int p = 5;
+        double k = 3;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenP13K18() {
+        double expected = 2.1066481994459836;
+        int p = 13;
+        double k = 18;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
