8233de35eb75babdd0ddad6ddfd496244ed72ac7
Khryashchev Evgeniy Olegovich <evgeniy.khryashchev@rosbank.ru>
2021-08-12 22:59:45 +0500

    4.2 Ternary condition

---
+++ test/ru/job4j/condition/MaxTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MaxTest {
+
+    @Test
+    public void whenMax1To2Then2() {
+        int left = 1;
+        int right = 2;
+        int result = Max.max(left, right);
+        int expected = 2;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax10To0Then10() {
+        int left = 10;
+        int right = 0;
+        int result = Max.max(left, right);
+        int expected = 10;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax3To5Then5() {
+        int left = 3;
+        int right = 5;
+        int result = Max.max(left, right);
+        int expected = 5;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax7To7Then7() {
+        int left = 7;
+        int right = 7;
+        int result = Max.max(left, right);
+        int expected = 7;
+        Assert.assertEquals(result, expected);
+    }
+}
