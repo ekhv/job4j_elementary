e545b36ad0d57d13ea41267f5c433d6df3fa83b7
Khryashchev Evgeniy Olegovich <evgeniy.khryashchev@rosbank.ru>
2021-08-12 10:31:36 +0500

    3. Tests for determining the distance between points in a coordinate system

---
+++ test/ru/job4j/condition/PointTest.java
@@ -0,0 +1,63 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PointTest {
+    @Test
+    public void when00to20() {
+        double expected = 2;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when01to20() {
+        double expected = 2.23606797749979;
+        int x1 = 0;
+        int y1 = 1;
+        int x2 = 2;
+        int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when11to20() {
+        double expected = 1.4142135623730951;
+        int x1 = 1;
+        int y1 = 1;
+        int x2 = 2;
+        int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when12to21() {
+        double expected = 1.4142135623730951;
+        int x1 = 1;
+        int y1 = 2;
+        int x2 = 2;
+        int y2 = 1;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when00to11() {
+        double expected = 1.4142135623730951;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 1;
+        int y2 = 1;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
