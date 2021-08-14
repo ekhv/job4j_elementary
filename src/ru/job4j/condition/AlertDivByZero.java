4df7984814f2f036060b972073b2c55cb6a127d4
Evgeniy Khryashchev <e.khryashchev@outlook.com>
2021-08-15 00:30:16 +0500

    refactoring

--- src/ru/job4j/condition/AlertDivByZero.java
+++ src/ru/job4j/condition/AlertDivByZero.java
@@ -9,7 +9,7 @@ public class AlertDivByZero {
     public static void possibleDiv(int number) {
         if (number == 0) {
             System.out.println("Could not div by 0.");
-        }else if(number < 0){
+        } else if (number < 0) {
             System.out.println("This is negative numbers");
         }
     }
