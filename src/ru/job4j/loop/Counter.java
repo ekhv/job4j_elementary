c62c6d151da399fe37a3b421b73827a8d7ff27e5
Evgeniy Khryashchev <e.khryashchev@outlook.com>
2021-08-16 21:50:18 +0500

    5.0.1. Sum of numbers

---
+++ src/ru/job4j/loop/Counter.java
@@ -0,0 +1,21 @@
+package ru.job4j.loop;
+
+public class Counter {
+    public static int sum(int start, int finish) {
+        int num;
+        int sum = 0;
+        for (num = start; num <= finish; num++) {
+            sum = sum + num;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(0, 10));
+        System.out.println(sum(3, 8));
+        System.out.println(sum(1, 1));
+        System.out.println(sum(0, 20));
+        System.out.println(sum(-1, 3));
+        System.out.println(sum(-3, -1));
+    }
+}
