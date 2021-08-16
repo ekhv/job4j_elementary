c2b7dda57bc2d4f6ad8211a0e71300d2c257476f
Evgeniy Khryashchev <e.khryashchev@outlook.com>
2021-08-16 21:31:13 +0500

    fixed value validation

--- src/ru/job4j/condition/ChessBoard.java
+++ src/ru/job4j/condition/ChessBoard.java
@@ -3,12 +3,14 @@ package ru.job4j.condition;
 public class ChessBoard {
     public static int way(int x1, int y1, int x2, int y2) {
         int rsl = 0;
-        boolean xSpan = 0 <= x1 * x2 && x1 * x2 <= 7 * 7;
-        boolean ySpan = 0 <= y1 * y2 && y1 * y2 <= 7 * 7;
-        boolean positiveSpan = x1 * y1 * x2 * y2 >= 0;
         boolean diagonalMovement = (Math.abs(x2 - x1) + Math.abs(y2 - y1)) % 2 == 0;
 
-        if (diagonalMovement && positiveSpan && xSpan && ySpan) {
+        if (0 <= x1 && x1 <= 7 &&
+                0 <= x2 && x2 <= 7 &&
+                0 <= y1 && y1 <= 7 &&
+                0 <= y2 && y2 <= 7 &&
+                diagonalMovement
+        ) {
             rsl = Math.abs(x2 - x1);
         }
         return rsl;
