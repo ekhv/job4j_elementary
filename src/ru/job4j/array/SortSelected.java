8646f67b6cd0ae5b4854db98415e2582d5c5513e
Evgeniy Khryashchev <e.khryashchev@outlook.com>
2021-08-23 10:12:37 +0500

    swap method

--- src/ru/job4j/array/SortSelected.java
+++ src/ru/job4j/array/SortSelected.java
@@ -1,16 +1,11 @@
 package ru.job4j.array;
 
-import java.util.Arrays;
-
 public class SortSelected {
-
     public static int[] sort(int[] data) {
         for (int i = 0; i < data.length; i++) {
-            int tmp = data[i];
             int min = MinDiapason.findMin(data, i, data.length - 1);
             int index = FindLoop.indexOf(data, min, i, data.length - 1);
-            data[i] = min;
-            data[index] = tmp;
+            SwitchArray.swap(data, index, i);
         }
         return data;
     }
